fb6029a5a8048eb29cc008782fbc43b1f6d9211f
Lucas Walter <wsacul@gmail.com>
2019-09-16 20:33:17 -0700

    basic ddr tf control

---
+++ scripts/ddr_tf.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python
+# Copyright 2019 Lucas Walter
+# BSD Licensed
+
+import rospy
+import tf
+import tf2_ros
+
+from ddynamic_reconfigure_python.ddynamic_reconfigure import DDynamicReconfigure
+from geometry_msgs.msg import TransformStamped
+from tf2_msgs.msg import TFMessage
+
+def transform_stamped(parent, child, x=0.0, y=0.0, z=0.0, roll=0.0, pitch=0.0, yaw=0.0):
+    ts = TransformStamped()
+    ts.header.frame_id = parent
+    ts.child_frame_id = child
+    ts.transform.translation.x = x
+    ts.transform.translation.y = y
+    ts.transform.translation.z = z
+    quat = tf.transformations.quaternion_from_euler(roll, pitch, yaw)
+    ts.transform.rotation.x = quat[0]
+    ts.transform.rotation.y = quat[1]
+    ts.transform.rotation.z = quat[2]
+    ts.transform.rotation.w = quat[3]
+    return ts
+
+class DDRtoTF(object):
+    def __init__(self):
+        self.tf_pub = rospy.Publisher('/tf', TFMessage, queue_size=4)
+
+        self.config = None
+        self.ddr = DDynamicReconfigure("")
+        self.ddr.add_variable("frame_id", "frame id", "map")
+        self.ddr.add_variable("child_frame_id", "frame id", "frame")
+        scale = rospy.get_param("~scale", 10.0)
+        self.ddr.add_variable("x", "x", 0.0, -scale, scale)
+        self.ddr.add_variable("y", "y", 0.0, -scale, scale)
+        self.ddr.add_variable("z", "z", 0.0, -scale, scale)
+        scale = rospy.get_param("~angle_scale", 3.2)
+        self.ddr.add_variable("roll", "roll", 0.0, -scale, scale)
+        self.ddr.add_variable("pitch", "pitch", 0.0, -scale, scale)
+        self.ddr.add_variable("yaw", "yaw", 0.0, -scale, scale)
+        self.ddr.start(self.config_callback)
+        self.timer = rospy.Timer(rospy.Duration(0.1), self.update)
+
+    def config_callback(self, config, level):
+        self.config = config
+        return config
+
+    def update(self, event):
+        config = self.config
+        ts = transform_stamped(config.frame_id, config.child_frame_id,
+                               config.x, config.y, config.z,
+                               config.roll, config.pitch, config.yaw)
+        ts.header.stamp = event.current_real
+
+        tfm = TFMessage()
+        tfm.transforms.append(ts)
+        self.tf_pub.publish(tfm)
+
+if __name__ == '__main__':
+    rospy.init_node('ddr_to_tf')
+    ddr_to_tf = DDRtoTF()
+    rospy.spin()
